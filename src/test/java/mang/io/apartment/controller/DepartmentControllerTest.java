d5d73db7b74c9fc965cc22222ee596cf751a30da
Mamang touthang <huebart6@gmail.com>
2022-04-02 16:46:07 +0530

    Repository testing with dataJpaTest and TestEntityManager

---
+++ src/test/java/mang/io/apartment/controller/DepartmentControllerTest.java
@@ -0,0 +1,40 @@
+package mang.io.apartment.controller;
+
+import mang.io.apartment.entity.Department;
+import mang.io.apartment.repository.DepartmentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class DepartmentControllerTest {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @BeforeEach
+    void setUp(){
+        Department department = Department.builder()
+                .departmentName("Electronic engg")
+                .departmentCode("EC")
+                .departmentAddress("Bangalore")
+                .build();
+        entityManager.persist(department);
+    }
+
+
+    @Test
+    @DisplayName("Repository test")
+    public void whenFindById_thenReturnDepartment(){
+        Department department = departmentRepository.findById(1L).get();
+        assertEquals(department.getDepartmentName(), "Electronic engg");
+    }
+}
